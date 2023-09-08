7d6c15979303fc0f5fcb86fbd812c420b1eeb1e0
viperovm <viperovm@gmail.com>
2023-09-08 15:31:03 +0300

    tasks rename

--- mail/tasks.py
+++ mail/tasks.py
@@ -14,47 +14,35 @@ from django.template.loader import get_template
 
 @shared_task()
 def send_message(mail_id):
-    print('Таска сработала')
 
-    Mail.objects.filter(pk=mail_id).update(counter=F('counter')+1)
+    obj = Mail.objects.get(pk=mail_id)
 
-    # obj.counter = 200
-    # obj.text = obj
-    # obj.save()
+    context = {
+        'data': obj,
+        'year': datetime.date.today().year,
+    }
+    html = get_template('mailing_list_template_html.html').render(context)
+    text = get_template('mailing_list_template_text.html').render(context)
 
-    # print(obj)
-    #
-    # context = {
-    #     'data': obj,
-    #     'year': datetime.date.today().year,
-    # }
-    # html = get_template('mailing_list_template_html.html').render(context)
-    # text = get_template('mailing_list_template_text.html').render(context)
-    #
-    # if obj.test_mailing:
-    #     e_mail_list = UserList.objects.filter(test_user=True).values_list('email', flat=True)
-    # else:
-    #     e_mail_list = UserList.objects.values_list('email', flat=True)
-    #
-    # for index, e_mail in enumerate(e_mail_list):
-    #     try:
-    #         send_mail(
-    #             obj.subject,
-    #             text,
-    #             'Nordic Way магазин комбинезонов <sales@nordicway.ru>',
-    #             [e_mail],
-    #             html_message=html,
-    #             fail_silently=False,
-    #         )
-    #         obj.counter += 1
-    #         obj.save()
-    #     except Exception as e:
-    #         error = MassMailErrors(subject=obj.subject, email=e_mail, text=e)
-    #         error.save()
-    #         print(e)
+    if obj.test_mailing:
+        e_mail_list = UserList.objects.filter(test_user=True).values_list('email', flat=True)
+    else:
+        e_mail_list = UserList.objects.values_list('email', flat=True)
 
-
-
+    for index, e_mail in enumerate(e_mail_list):
+        try:
+            send_mail(
+                obj.subject,
+                text,
+                'Nordic Way магазин комбинезонов <sales@nordicway.ru>',
+                [e_mail],
+                html_message=html,
+                fail_silently=False,
+            )
+            Mail.objects.filter(pk=mail_id).update(counter=F('counter')+1)
+        except Exception as e:
+            error = MassMailErrors(subject=obj.subject, email=e_mail, text=e)
+            error.save()
 
 
 
